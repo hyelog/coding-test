f007a31bd1bae552ab061f694af991b203f10860
Audrey <hjsuccess713@gmail.com>
2023-09-13 09:23:03 -0400

    feat: today code

---
+++ data-structure/src/main/java/org/example/data/structure/three/Node.java
@@ -0,0 +1,10 @@
+package org.example.data.structure.three;
+
+public class Node {
+    public Node above;
+    public Node below;
+    public int value;
+    public Node(int value) {
+        this.value = value;
+    }
+}
